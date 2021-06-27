647f18bb8441c4296e3090245e9a286429598d90
displayScript <abmael_ninha20@hotmail.com>
2021-06-27 14:56:03 -0400

    Pagina de sala Admin

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Room } from './pages/Rooms';
 
 
 import { AuthContextProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
